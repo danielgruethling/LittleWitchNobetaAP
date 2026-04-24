91f3a1822533daed3112c98a6196370d503b3a29
espaspw <superpanamaworld@gmail.com>
2026-04-24 11:17:44 -0700

    Add util for referring to objects using their full path

--- LittleWitchNobetaAP/Utils/UnityUtils.cs
+++ LittleWitchNobetaAP/Utils/UnityUtils.cs
@@ -23,4 +23,26 @@ public static class UnityUtils
 
         return gameObject != null ? gameObject.GetComponent<TComponent>() : default;
     }
+    
+    public static String GetObjectPath(GameObject obj) {
+        string path = "/" + obj.name;
+        Transform current = obj.transform;
+        while (current.parent != null)
+        {
+            current = current.parent;
+            path = "/" + current.name + path;
+        }
+        return path;
+    }
+    
+    public static GameObject? FindObjectByPath(string path)
+    {
+        var root = GameObject.Find(path.Split('/')[1]); 
+        if (root is null) return null;
+        
+        if (path.Split('/').Length == 2) return root;
+    
+        var transform = root.transform.Find(path.Substring(path.IndexOf('/', 1) + 1));
+        return transform?.gameObject;
+    }
 }
